9dce7a19a3bd8e6ab21b6c4ce8cae729eacce758
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-06-05 14:55:44 +0100

    removed framework as having issue

---
+++ Sources/coursesU-iOS-templates/ui/Budget/CTA/CUBudgetCallToAction.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by didi on 6/5/23.
+//
+
+import SwiftUI
+//import miamCore
+//import MiamIOSFramework
+
+@available(iOS 14, *)
+struct CUBudgetCallToAction: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+@available(iOS 14, *)
+struct CUBudgetCallToAction_Previews: PreviewProvider {
+    static var previews: some View {
+        CUBudgetCallToAction()
+    }
+}
