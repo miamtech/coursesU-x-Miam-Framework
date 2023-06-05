c230b527ad77a43fe0d4afd7942cb82a3136af9e
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-06-05 16:38:28 +0100

    gave examples of how to use component in previews

--- Sources/coursesU-iOS-templates/ui/Budget/BudgetBackground.swift
+++ Sources/coursesU-iOS-templates/ui/Budget/BudgetBackground.swift
@@ -24,6 +24,40 @@ struct BudgetBackground: View {
 @available(iOS 14, *)
 struct BudgetBackground_Previews: PreviewProvider {
     static var previews: some View {
-        BudgetBackground()
+        // when scrolling
+        ScrollView {
+            ZStack(alignment: .top) {
+                BudgetBackground()
+                VStack {
+                    ForEach(0..<20) { index in
+                        Text("Row \(index)")
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .padding(.horizontal)
+                    }
+                }
+                .padding(.top, UIScreen.screenHeight * 0.2)
+            }
+        }
+        
+        // when static
+        
+            ZStack(alignment: .top) {
+                BudgetBackground()
+                VStack {
+                    
+                        Text("Stagnant")
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .padding(.horizontal)
+                    
+                }
+                .padding(.top, UIScreen.screenHeight * 0.2)
+            
+        }
     }
 }
