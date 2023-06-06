fa7594381f4e3c8372c9bdbc48042837c149f55d
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-06-06 12:00:36 +0100

    added boilerplate for Form, cannot progress far without knowing functionality

--- Sources/coursesU-iOS-templates/ui/Budget/Form/CoursesUBudgetForm.swift
+++ Sources/coursesU-iOS-templates/ui/Budget/Form/CoursesUBudgetForm.swift
@@ -17,10 +17,65 @@ public struct CoursesUBudgetForm: BudgetForm {
     let dimension = Dimension.sharedInstance
     public init() {}
     public func content(budgetInfos: BudgetInfos? = nil, isFetchingRecipes: Bool, onBudgetChanged: @escaping (BudgetInfos) -> Void, onFormValidated: @escaping (BudgetInfos) -> Void) -> some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Choissez vos repas de la semaine ou du mois selon votre budget")
+                .coursesUFontStyle(style: CoursesUFontStyleProvider.sharedInstance.bodyBigBoldStyle)
+            Divider()
+            // TODO: localize
+            CoursesUBudgetFormButtons(icon: Image.miamNeutralImage(icon: .guests), title: "Mon budget max")
+            { _ in }
+            Divider()
+            // TODO: localize
+            CoursesUBudgetFormButtons(icon: Image.miamNeutralImage(icon: .guests), title: "Nombre de personnes")
+            { _ in }
+            Divider()
+            // TODO: localize
+            CoursesUBudgetFormButtons(icon: Image.miamNeutralImage(icon: .guests), title: "Nombre de repas")
+            { _ in }
+            Divider()
+        }
     }
+    
 }
 
+@available(iOS 14, *)
+struct CoursesUBudgetFormButtons: View {
+    @SwiftUI.State public var value: Int = 0
+    let icon: Image
+    let title: String
+    let minValue: Int = 1
+    let maxValue: Int = 10
+    public var onStepperChanged: (Int) -> Void
+    init(defaultValue: Int = 0, icon: Image, title:String, minValue: Int = 1, maxValue: Int = 10, onStepperChanged: @escaping (Int) -> Void) {
+        _value = State(initialValue: defaultValue)
+        self.icon = icon
+        self.title = title
+        self.onStepperChanged = onStepperChanged
+    }
+    let dimension = Dimension.sharedInstance
+    var body: some View {
+        HStack() {
+            
+            icon
+                .renderingMode(.template)
+                .resizable()
+                .frame(width: dimension.lButtonHeight, height: dimension.lButtonHeight)
+                .padding(.horizontal, dimension.sPadding)
+                .foregroundColor(Color.miamColor(.black))
+            VStack(alignment: .leading, spacing: 5) {
+                Text(title)
+                    .coursesUFontStyle(style: CoursesUFontStyleProvider.sharedInstance.bodyStyle)
+                Text(String(Int(value)))
+                    .coursesUFontStyle(style: CoursesUFontStyleProvider.sharedInstance.bodyBigBoldStyle)
+            }
+//            .padding(.leading, dimension.mPadding)
+            
+            Spacer()
+        }
+        
+            
+    }
+}
 @available(iOS 14, *)
 struct CoursesUBudgetForm_Previews: PreviewProvider {
     static var previews: some View {
