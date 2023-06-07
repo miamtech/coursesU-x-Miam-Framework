bc062753cb2a69085dd47d303ba0bc4b10ca259d
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-06-07 10:27:01 +0100

    Created Recipe card placeholder

---
+++ Sources/coursesU-iOS-templates/ui/Budget/Recipe/CoursesUBudgetRecipePlaceholder.swift
@@ -0,0 +1,67 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by didi on 6/7/23.
+//
+
+import SwiftUI
+import miamCore
+import MiamIOSFramework
+
+@available(iOS 14, *)
+public struct CoursesUBudgetRecipePlaceholder: BudgetRecipePlaceholder {
+    private let recipeCardSize = CGSize(width: 175.0, height: 175.0)
+    private let dimension = Dimension.sharedInstance
+    public init() {}
+    public func content(onTapGesture: @escaping () -> Void) -> some View {
+        ZStack(alignment: .topLeading) {
+            VStack(spacing: dimension.lPadding) {
+                Button {
+                    onTapGesture()
+                } label: {
+                    Circle()
+                        .foregroundColor(Color.primaryColor)
+                        .frame(width: dimension.lButtonHeight, height: dimension.lButtonHeight)
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 3)
+                        )
+                        .overlay(
+                            Image(systemName: "plus")
+                                .resizable()
+                                .foregroundColor(Color.white)
+                                .frame(width: dimension.mButtonHeight, height: dimension.mButtonHeight)
+                        )
+                }
+                .buttonStyle(PlainButtonStyle())
+                //                    Text(Localization.myBudget.addMealIdea.localised)
+                // TODO: localize
+                Text("Ajouter une idée repas")
+                    .foregroundColor(Color.white)
+                    .coursesUFontStyle(style: CoursesUFontStyleProvider.sharedInstance.subtitleStyle)
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: dimension.mealPlannerRecipeCardHeight)
+            .background(Color.primaryColor)
+            .cornerRadius(dimension.mCornerRadius)
+            
+            YellowOffCenterNumber(number: "3")
+                .padding(10)
+            
+        }
+    }
+}
+
+@available(iOS 14, *)
+struct CoursesUBudgetRecipePlaceholder_Preview: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.budgetBackgroundColor
+            CoursesUBudgetRecipePlaceholder().content {
+                
+            }
+        }
+        
+    }
+}
