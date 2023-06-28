5bb354e40ae692ade415c336708a38638daab8bb
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-06-28 11:29:33 +0100

    Added Call To Action button, added it to Storyboard

---
+++ Sources/CoursesUIOSTemplate/ui/MealPlanner/CTA/CoursesUMealPlannerCallToAction.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by didi on 6/5/23.
+//
+
+import SwiftUI
+import miamCore
+import MiamIOSFramework
+
+@available(iOS 14, *)
+public struct CoursesUMealPlannerCallToAction: BudgetCallToAction {
+   
+    public init() {}
+    let screen = UIScreen.screenSize
+    public func content(onTapGesture: @escaping () -> Void) -> some View {
+        Image(packageResource: "CTAImage", ofType: "png")
+            .resizable()
+            .scaledToFit()
+            .frame(maxWidth: .infinity)
+            .padding(Dimension.sharedInstance.mPadding)
+            .onTapGesture {
+                onTapGesture()
+            }
+    }
+}
+
+@available(iOS 14, *)
+struct CoursesUMealPlannerCallToAction_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesUMealPlannerCallToAction().content {
+            print("hello world")
+        }
+    }
+}
