2a1567e4cdfcf1d322f489d485843550a4a7d60f
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-07-03 10:07:52 +0100

    changed name from CoursesUIOSTemplates to CoursesUxMiamFramework

--- Package.swift
+++ Package.swift
@@ -4,27 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "CoursesUIOSTemplate",
+    name: "CoursesUxMiamFramework",
     platforms: [
         .iOS(.v12),
       ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CoursesUIOSTemplate",
-            targets: ["CoursesUIOSTemplate"]),
+            name: "CoursesUxMiamFramework",
+            targets: ["CoursesUxMiamFramework"]),
     ],
     dependencies: [
             // The package dependency is defined as a local path.
 //        .package(url: "https://github.com/miamtech/miam-sdk", from: "3.0.0"),
-        .package(url: "https://github.com/miamtech/miam-sdk", branch: "main"),
-//        .package(path: "../../miam-sdk"),
+//        .package(url: "https://github.com/miamtech/miam-sdk", branch: "main"),
+        .package(path: "../../miam-sdk"),
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CoursesUIOSTemplate",
+            name: "CoursesUxMiamFramework",
             dependencies: [
                     .product(name: "MiamIOSFramework", package: "miam-sdk")
                   ],
@@ -35,6 +35,6 @@ let package = Package(
 //                    ]),
         .testTarget(
             name: "coursesU-iOS-templatesTests",
-            dependencies: ["CoursesUIOSTemplate"]),
+            dependencies: ["CoursesUxMiamFramework"]),
     ]
 )
--- Tests/coursesU-iOS-templatesTests/coursesU_iOS_templatesTests.swift
+++ Tests/coursesU-iOS-templatesTests/coursesU_iOS_templatesTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import CoursesUIOSTemplate
+@testable import CoursesUxMiamFramework
 
 final class coursesU_iOS_templatesTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CoursesUIOSTemplate().text, "Hello, World!")
+        XCTAssertEqual(CoursesUxMiamFramework().text, "Hello, World!")
     }
 }
