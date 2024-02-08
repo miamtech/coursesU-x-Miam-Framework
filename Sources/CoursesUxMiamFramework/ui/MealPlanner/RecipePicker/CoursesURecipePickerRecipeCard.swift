0cb123cc72cf5f20cbd1d26943b6e8567342cc23
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-02-08 18:03:46 +0100

    add recipe picker recipe card

---
+++ Sources/CoursesUxMiamFramework/ui/MealPlanner/RecipePicker/CoursesURecipePickerRecipeCard.swift
@@ -0,0 +1,88 @@
+//
+//  CoursesURecipePickerRecipeCard.swift
+//
+//
+//  Created by Diarmuid McGonagle on 08/02/2024.
+//
+
+import SwiftUI
+import miamCore
+import MiamIOSFramework
+
+@available(iOS 14, *)
+public struct CoursesURecipePickerRecipeCard: CatalogRecipeCardProtocol {
+    public init() {}
+    public func content(params: CatalogRecipeCardParameters) -> some View {
+        let dimensions = Dimension.sharedInstance
+        var ctaAction: (String) -> Void {
+            return params.isCurrentlyInBasket ? params.onShowRecipeDetails : params.onAddToBasket
+        }
+        let recipePrice = params.recipePrice * Double(params.numberOfGuests)
+        return VStack(spacing: 0.0) {
+            VStack(spacing: 0.0) {
+                ZStack(alignment: .topTrailing) {
+                    AsyncImage(url: params.recipe.pictureURL) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .padding(0)
+                            .frame(minWidth: 0, maxWidth: .infinity, maxHeight: .infinity)
+                    }.frame(height: 150.0)
+                        .clipped()
+                    
+                    CoursesULikeButton(recipeId: params.recipe.id)
+                        .padding(dimensions.mPadding)
+                }
+                VStack(spacing: dimensions.mPadding) {
+                    Text(params.recipe.title + "\n")
+                        .coursesUFontStyle(style: CoursesUFontStyleProvider().bodyMediumBoldStyle)
+                        .lineLimit(2)
+                        .padding(.top, dimensions.sPadding)
+                    HStack(spacing: dimensions.mPadding) {
+                        CoursesURecipePreparationTime(duration: params.recipe.cookingTimeIos)
+                        Divider()
+                            .frame(width: 5, height: 40)
+                        CoursesURecipeDifficulty(difficulty: params.recipe.difficulty)
+                    }
+                    ZStack {
+                        if params.recipePrice > 0 {
+                            RecapPriceForRecipes(priceAmount: recipePrice.currencyFormatted)
+                        } else {
+                            ProgressLoader(color: .primaryColor)
+                                .scaleEffect(0.3)
+                        }
+                    }.frame(height: 25)
+                    Divider()
+                    CoursesUButtonStyle(backgroundColor: Color.primaryColor, content: {
+                        HStack {
+                            Image(systemName: "plus")
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                                .foregroundColor(Color.white)
+                            Text(Localization.recipe.add.localised)
+                                .foregroundColor(Color.white)
+                                .coursesUFontStyle(style: CoursesUFontStyleProvider.sharedInstance.bodyStyle)
+                        }
+                        
+                    }, buttonAction: {
+                        //                        actions.addToBasket(recipeInfos.recipe.id)
+                        ctaAction(params.recipe.id)
+                    })
+                    .padding(.bottom, dimensions.sPadding)
+                    .frame(maxHeight: 50)
+                }
+                .padding(.horizontal, dimensions.mPadding)
+                .frame(maxHeight: .infinity)
+            }
+        }
+        .padding(0)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(12.0)
+        .overlay(RoundedRectangle(cornerRadius: 12.0).stroke(Color.lightGray, lineWidth: 1.0))
+        .onTapGesture {
+            params.onShowRecipeDetails(params.recipe.id)
+        }
+        .frame(height: params.recipeCardDimensions.height)
+    }
+}
