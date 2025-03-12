dd1d22b335e9731f2c2b54195b4b1708a02ed200
walerowicz-miam <149375102+walerowicz-miam@users.noreply.github.com>
2025-03-12 13:36:53 +0100

    update filters

--- Sources/CoursesUxMiamFramework/ui/Catalog/Filters/CoursesUfiltersView.swift
+++ Sources/CoursesUxMiamFramework/ui/Catalog/Filters/CoursesUfiltersView.swift
@@ -4,36 +4,3 @@
 //
 //  Created by Damien Walerowicz on 20/03/2024.
 //
-
-import Foundation
-import mealzcore
-import MealziOSSDK
-import SwiftUI
-
-
-@available(iOS 14.0, *)
-public struct CoursesUFilterView: FiltersHeaderProtocol {
-    public init() {}
-    public func content(params: FiltersHeaderParameters) -> some View  {
-        HStack {
-            Button(action: params.onCloseFilters, label: {
-                Image.mealzIcon(icon: .arrow)
-                    .renderingMode(.template)
-                    .rotationEffect(Angle(degrees: 180))
-            })
-            .frame(width: 40, height: 40)
-            .overlay(Circle().stroke(Color.white, lineWidth: 1))
-            .foregroundColor(Color.mealzColor(.white))
-            .clipShape(Circle()).padding()
-            
-            Text(Localization.filter.pageTitle.localised)
-                .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.titleStyle)
-                .foregroundColor(Color.mealzColor(.white))
-            
-            Spacer()
-        }
-        .frame(maxWidth: .infinity)
-        .background(Color.mealzColor(.primary))
-    }
-    
-}
